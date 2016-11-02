fe4a804682a62e2a9371402e7dd3d63073a4d696
unknown <shineky7@sina.cn>
2016-11-02 19:31:04 +0800

    MVC 2.0

--- index.php
+++ index.php
@@ -4,18 +4,26 @@ header("content-type:text/html;charset=utf-8");
 // 导入配置文件 + model类
 require './configs/config.php';
 
-require './models/Model.class.php';
-
-require './controllers/IndexController.class.php';
-require './controllers/GoodsController.class.php';
-require './controllers/UserController.class.php';
+// 自动加载类
+function __autoload($classname)
+{
+	if (file_exists("./model/{$classname}.class.php")) {
+		require "./model/{$classname}.class.php";
+	}elseif (file_exists("./controllers/{$classname}.class.php")) {
+		require "./controllers/{$classname}.class.php";
+	}else{
+		header("HTTP/1.0 404 not found");
+		echo "<h1>404 NOT FOUND</h1>";
+		exit;
+	}
+}
 
 // 获取用户参数
 // 获取控制器名 类名
 $c = (!empty($_GET['c'])?$_GET['c']:'Index');
 
 // 获取操作名   方法名
-$a = (!empty($_GET['a'])?$_GET['a']:'Index');
+$a = (!empty($_GET['a'])?$_GET['a']:'index');
 
 // 拼装类名
 $classname = $c.'Controller';
